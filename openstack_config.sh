1c96ad9fcefe237557c1bbd5cf5f02f638335835
Bk <94864052+bksuup@users.noreply.github.com>
2023-02-08 08:40:05 +0100

    Update openstack_config.sh

--- openstack_config.sh
+++ openstack_config.sh
@@ -18,4 +18,4 @@ echo \
   
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
